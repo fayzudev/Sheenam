64f1f8951fe59b98cc018cec2dbdf9e248924949
frakhimov505 <87068169+frakhimov505@users.noreply.github.com>
2023-01-18 20:53:50 +0500

    Changed

---
+++ Sheenam.Api/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿//==============================================
+// Copyright (c) Coalition Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==============================================
+
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Sheenam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+
+    {
+        [HttpGet]
+        public ActionResult <string> Get() => 
+            Ok("Hello Mario, the Princess in another castle");
+    }
+}
